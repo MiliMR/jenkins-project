8787b43f15c5fd7c0bcac5d6b091c49dfa98cbb4
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-03 13:38:28 -0500

    Update Main.scala

--- dotty-example-project/src/main/scala/Main.scala
+++ dotty-example-project/src/main/scala/Main.scala
@@ -2,22 +2,15 @@
 object Main {
 
   def main(args: Array[String]): Unit = {
-
     runExample("Trait Params")(TraitParams.test)
-
     runExample("Enum Types")(EnumTypes.test)
-
     runExample("Context Queries")(ContextQueries.test)
-
     runExample("Implied Instances")(ImpliedInstances.test)
-
     runExample("Conversion")(Conversion.test)
-
     runExample("Union Types")(UnionTypes.test)
-
     runExample("Intersection Types")(IntersectionTypes.test)
 
-    runExample("Type Lambda")(TypeLambdas.test)
+    //runExample("Type Lambda")(TypeLambdas.test)
 
     runExample("Multiversal Equality")(MultiversalEquality.test)
 
