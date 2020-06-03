237887035991ed6c97d31e3003e4ae7520acd6cf
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-03 13:15:10 -0500

    Update EnumTypes.scala

--- dotty-example-project/src/main/scala/EnumTypes.scala
+++ dotty-example-project/src/main/scala/EnumTypes.scala
@@ -2,17 +2,14 @@
   * Enum Types: http://dotty.epfl.ch/docs/reference/enums/adts.html
   */
 object EnumTypes {
-
   enum ListEnum[+A] {
     case Cons(h: A, t: ListEnum[A])
     case Empty
   }
-
   enum Planet(mass: Double, radius: Double) {
     private final val G = 6.67300E-11
     def surfaceGravity = G * mass / (radius * radius)
     def surfaceWeight(otherMass: Double) =  otherMass * surfaceGravity
-
     case Mercury extends Planet(3.303e+23, 2.4397e6)
     case Venus   extends Planet(4.869e+24, 6.0518e6)
     case Earth   extends Planet(5.976e+24, 6.37814e6)
@@ -22,7 +19,6 @@ object EnumTypes {
     case Uranus  extends Planet(8.686e+25, 2.5559e7)
     case Neptune extends Planet(1.024e+26, 2.4746e7)
   }
-
   def test: Unit = {
 
     val emptyList = ListEnum.Empty
