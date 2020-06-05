24e753267c9dc478c161d42dbfdcbcecb0163791
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-05 17:28:32 -0500

    Update AutoParamTupling.scala

--- dotty-example-project/src/main/scala/AutoParamTupling.scala
+++ dotty-example-project/src/main/scala/AutoParamTupling.scala
@@ -6,7 +6,6 @@
 object AutoParamTupling {
 
   def test: Unit = {
-
     /**
       * In order to get thread safety, you need to put @volatile before lazy vals.
       * https://dotty.epfl.ch/docs/reference/changed-features/lazy-vals.html
