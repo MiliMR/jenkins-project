1da1676e913f3a3525727f5afa868cb4f1103f2b
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-11 11:12:05 -0500

    Update and rename analyzeDependencyCheckReport.js to analyzeDependencyCheckReport.test.js

---
+++ dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
@@ -0,0 +1,4 @@
+test('Search Vulnerabilities in DependencyCheckReport', async () => {
+    const file = require('../../../target/scala-0.24/dependency-check-report.html');
+    const newTodoInput = file.getElementById('vulnerableCount');
+});
