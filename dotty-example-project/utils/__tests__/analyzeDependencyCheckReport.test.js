4fb7c114e9bc9d04a46338e27e8d78f8b64abbf6
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-12 13:32:27 -0500

    Update analyzeDependencyCheckReport.test.js

--- dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
+++ dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
@@ -1,7 +1,7 @@
 const jsdom = require("jsdom");
 test('Search Vulnerabilities in DependencyCheckReport', async () => {
     var fs = require("fs");
-    var directory = process.env.REPORT_FILE;
+    var directory = ".."+process.env.REPORT_FILE;
     var document = fs.readFileSync(directory).toString();
     //var vulnerabilityTag = "vulnerabilityIds[confidence=\"HIGHEST\"]";
     var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]"
