68372eac3dd42a68b54f60bb453a55fe86f9ac74
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-12 09:57:52 -0500

    Update analyzeDependencyCheckReport.test.js

--- dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
+++ dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
@@ -3,7 +3,7 @@ test('Search Vulnerabilities in DependencyCheckReport', async () => {
     var fs = require("fs");
     var directory = "../target/scala-0.24/dependency-check-report.xml";
     var document = fs.readFileSync(directory).toString();
-    var vulnerabilityTag = "vulnerabilityIds[confidence=\"SMALL\"]";
+    var vulnerabilityTag = "vulnerabilityIds[confidence=\"HIGHEST\"]";
     const dom = new jsdom.JSDOM(document);
     expect(dom.window.document.querySelector(vulnerabilityTag)).toBeNull();
 });
