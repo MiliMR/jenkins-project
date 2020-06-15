fe3423e63866fc6152d64cc10dccd2f41ce746bc
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-15 18:16:16 -0500

    Update analizeDependencyReport.js

--- dotty-example-project/utils/analizeDependencyReport.js
+++ dotty-example-project/utils/analizeDependencyReport.js
@@ -1,22 +1,17 @@
 const jsdom = require("jsdom");
 require('dotenv').config();
 var fs = require("fs");
+var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
 var reportPath = process.env.REPORT_PATH_ANALYSIS;
-try{
-    var reportContent = fs.readFileSync(reportPath).toString();
-    var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
-    const dom = new jsdom.JSDOM(reportContent);
-    var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
-    console.log(matches.length + " " + process.env.MAX_VULNERABILITY);
-    if(Number(matches.length) > Number(process.env.MAX_VULNERABILITY)){
-        try{
-            throw new Error(" Expected: < "+ process.env.MAX_VULNERABILITY + "\n Received:   " + matches.length);
-        }catch(e){
-            console.log(e.message);
-        }
-    }else{
-        console.log("Test Suites: 1 passed") 
+var reportContent = fs.readFileSync(reportPath).toString();
+const dom = new jsdom.JSDOM(reportContent);
+var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
+if(Number(matches.length) > Number(process.env.MAX_VULNERABILITY)){
+    try{
+        throw new Error(" Expected: < "+ process.env.MAX_VULNERABILITY + "\n Received:   " + matches.length);
+    }catch(e){
+        console.log(e.message);
     }
-}catch(exception){
-    console.log("Invalid path : "+process.env.REPORT_PATH_ANALYSIS)
+}else{
+    console.log("Test Suites: 1 passed");
 }
