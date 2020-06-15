5f98c26cb68248d0070ca5d487291eb1e84ca413
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-15 10:33:18 -0500

    Update analyzeDependencyCheckReport.test.js

--- dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
+++ dotty-example-project/utils/__tests__/analyzeDependencyCheckReport.test.js
@@ -8,7 +8,7 @@ test('Search Vulnerabilities in DependencyCheckReport', async () => {
     var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
     const dom = new jsdom.JSDOM(reportContent);
     var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
-    expect(Number(matches.length)).toBeLessThan(process.env.MAX_VULNERABILITY);
+    expect(Number(matches.length)).toBeLessThan(Number(process.env.MAX_VULNERABILITY));
     
     /*if(matches.length > process.env.MAX_VULNERABILITY){
         process.env['VULNERABILITY_DETECTED'] = true;
