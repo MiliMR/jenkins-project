d7a1d51acd85d26338b0734a07d8801b21daddb0
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-15 18:30:19 -0500

    Update analizeDependencyReport.js

--- dotty-example-project/utils/analizeDependencyReport.js
+++ dotty-example-project/utils/analizeDependencyReport.js
@@ -3,12 +3,19 @@ require('dotenv').config();
 var fs = require("fs");
 var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
 var reportPath = process.env.REPORT_PATH_ANALYSIS;
-var reportContent = fs.readFileSync(reportPath).toString();
-const dom = new jsdom.JSDOM(reportContent);
-var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
-if(Number(matches.length) > Number(process.env.MAX_VULNERABILITY)){
-    console.log(" Expected: < "+ process.env.MAX_VULNERABILITY + "\n Received:   " + matches.length);
-    throw new Error();
-}else{
-    console.log("Test Suites: 1 passed");
+try{
+    var reportContent = fs.readFileSync(reportPath).toString();
+    const dom = new jsdom.JSDOM(reportContent);
+    var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
+    if(Number(matches.length) > Number(process.env.MAX_VULNERABILITY)){
+        throw new Error(true);
+    }else{
+        console.log("Test Suites: 1 passed");
+    }
+}catch(exception){
+    if (excepcion) {
+      console.log(" Expected: < "+ process.env.MAX_VULNERABILITY + "\n Received:   " + matches.length);
+      throw excepcion;
+   }
+    console.log("Invalid path : " + reportPath);
 }
