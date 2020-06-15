20bb57c4fbdda1a0ce7839e1c8f63f52343969dc
MiliMR <49690936+MiliMR@users.noreply.github.com>
2020-06-15 17:52:52 -0500

    Update analizeDependencyReport.js

--- dotty-example-project/utils/analizeDependencyReport.js
+++ dotty-example-project/utils/analizeDependencyReport.js
@@ -4,9 +4,13 @@ var fs = require("fs");
 var reportPath = process.env.REPORT_PATH_ANALYSIS;
 try{
     var reportContent = fs.readFileSync(reportPath).toString();
+    console.log('1');
     var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
+    console.log('1');
     const dom = new jsdom.JSDOM(reportContent);
+    console.log('1');
     var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
+    console.log('1');
     if(Number(matches.length) > Number(process.env.MAX_VULNERABILITY)){
        throw new Error(" Expected: < "+ process.env.MAX_VULNERABILITY + "\n Received:   " + matches.length);
     }
