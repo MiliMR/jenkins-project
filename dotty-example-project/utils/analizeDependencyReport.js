e70ff97402566497b0bddb60bf4f73063c268f54
MMotta-zAgile <mmotta@zagile.com>
2020-06-16 12:13:37 -0500

    Update Changes

--- dotty-example-project/utils/analizeDependencyReport.js
+++ dotty-example-project/utils/analizeDependencyReport.js
@@ -2,20 +2,23 @@ const jsdom = require("jsdom");
 require('dotenv').config();
 var fs = require("fs");
 var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
-var reportPath = process.env.REPORT_PATH_ANALYSIS;
+var reportPath = process.env.REPORT_PATH;
 try{
-    var reportContent = fs.readFileSync(reportPath).toString();
-    const dom = new jsdom.JSDOM(reportContent);
-    var matches = dom.window.document.querySelectorAll(vulnerabilityTag);
-    if(Number(matches.length) > Number(process.env.MAX_VULNERABILITY)){
-        throw new Error(true);
-    }else{
-        console.log("Test Suites: 1 passed");
-    }
+  var reportContent = fs.readFileSync(reportPath).toString();
+  const dom = new jsdom.JSDOM(reportContent);
+  var match = dom.window.document.querySelector(vulnerabilityTag);
+  if(match == null){
+    console.log("Not found Vulnerabilities");
+  }else{
+    throw new Error('Detected');
+  }
 }catch(exception){
-    if (excepcion) {
-      console.log(" Expected: < "+ process.env.MAX_VULNERABILITY + "\n Received:   " + matches.length);
-      throw excepcion;
-   }
+  if (exception.message == 'Detected') {
+    console.log("Vulnerabilities Found");
+    throw exception;
+  }
+  else if (exception.code == 'ENOENT'){
     console.log("Invalid path : " + reportPath);
+    throw exception;
+  }
 }
