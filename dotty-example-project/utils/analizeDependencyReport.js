642c80e54743ab1a6e8fcb16481dc5a8cf0527e6
MMotta-zAgile <58340891+MMotta-zAgile@users.noreply.github.com>
2020-06-17 12:24:22 -0500

    Update analizeDependencyReport.js

--- dotty-example-project/utils/analizeDependencyReport.js
+++ dotty-example-project/utils/analizeDependencyReport.js
@@ -3,22 +3,16 @@ require('dotenv').config();
 var fs = require("fs");
 var vulnerabilityTag = "software[vulnerabilityIdMatched=\"true\"]";
 var reportPath = process.env.REPORT_PATH;
-try{
-  var reportContent = fs.readFileSync(reportPath).toString();
-  const dom = new jsdom.JSDOM(reportContent);
-  var match = dom.window.document.querySelector(vulnerabilityTag);
-  if(match == null){
-    console.log("Not found Vulnerabilities");
-  }else{
-    throw new Error('Detected');
-  }
-}catch(exception){
-  if (exception.message == 'Detected') {
-    console.log("Vulnerabilities Found");
+try {
+    var reportContent = fs.readFileSync(reportPath).toString();
+    const dom = new jsdom.JSDOM(reportContent);
+    var match = dom.window.document.querySelector(vulnerabilityTag);
+    if (match != null) {
+        console.log("Vulnerabilities Found");
+    }
+} catch (exception) {
+    if (exception.code == 'ENOENT') {
+        console.log("Invalid path : " + reportPath);
+    }
     throw exception;
-  }
-  else if (exception.code == 'ENOENT'){
-    console.log("Invalid path : " + reportPath);
-    throw exception;
-  }
 }
